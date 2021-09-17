90df24166ffc794d04af3f4be11e8f34ab431576
PALO <pavol.vasek20@gmail.com>
2021-09-17 11:33:14 +0200

    fix 2

--- cam_service.py
+++ cam_service.py
@@ -96,7 +96,7 @@ while True:
             
             if barcodes_in_order:
                 barcodes_in_order = sorted(barcodes_in_order, key=itemgetter(0))
-                barcodes_in_order = [x for x[2] in barcodes_in_order]
+                barcodes_in_order = [x[2] for x in barcodes_in_order]
                 write_time = str(datetime.now())
                 barcodes_in_order.insert(0,write_time)
                 write_data_into_file(barcodes_in_order)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from config import path_write,separator
+from operator import itemgetter
 
 # Zapis barcodov do súboru
 def write_data_into_file(barcodes_in_order):
@@ -8,8 +9,16 @@ def write_data_into_file(barcodes_in_order):
     w_file.write(separator.join(barcodes_in_order))
     w_file.close()
     return 0
+barcodes_in_order=[]
+barcodes_in_order.append([100,10,'barcode1'])
 
-write_time = str(datetime.now())
-barcode_data = ['barcode1','barcode181']
-barcode_data.insert(0,write_time)
-write_data_into_file(barcode_data)
+
+barcodes_in_order = sorted(barcodes_in_order, key=itemgetter(0))
+print(type(barcodes_in_order))
+if barcodes_in_order:
+    barcodes_in_order = [x[2] for x in barcodes_in_order]
+    write_time = str(datetime.now())
+    barcodes_in_order.insert(0,write_time)
+    write_data_into_file(barcodes_in_order)
+
+
